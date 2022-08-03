e4b242e685da00065ba3c2ac653c096fe3af1766
jmo707 <jmo707@gmail.com>
2022-08-03 14:13:36 -0300

    Progreso con el sondeo de strings en statsWords.sh

--- Menú.sh
+++ Menú.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+sleep 2s && echo
 
+read -p "Bienvenidx. Indique la ruta absoluta al archivo de texto que se utilizará como entrada: " INPUT
+echo "$INPUT"
+
+declare -a options=("statsWords.sh" "statsUsageWords.sh" "findNames.sh" "statsSentences.sh" "blankLinesCounter.sh")
+
+echo "${options[@]}"
